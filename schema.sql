768ad7a8782556cf29b2b4e4c9dbedf4dce1a93d
Sven Goerdes <Sven@Goerdes.com>
2024-11-25 20:06:03 +0000

    Changed up order of table creation and changed some naming convetions in some tables (they were initilayy wrong)

--- schema.sql
+++ schema.sql
@@ -1,14 +1,59 @@
-DROP SCHEMA LSR;
+DROP SCHEMA IF EXISTS LSR;
 CREATE SCHEMA IF NOT EXISTS LSR;
 
 USE LSR;
 
+CREATE TABLE IF NOT EXISTS country_table (
+    country_id varchar(4) NOT NULL,
+    country_name varchar(30) NOT NULL,
+    PRIMARY KEY (country_id)
+);
+
+CREATE TABLE IF NOT EXISTS state_table (
+    state_id varchar(4) NOT NULL,
+    state_name varchar(30) NOT NULL,
+    country_id varchar(4) NOT NULL,
+    PRIMARY KEY (state_id),
+    FOREIGN KEY (country_id) REFERENCES country_table(country_id)
+);
+
+CREATE TABLE IF NOT EXISTS city_table (
+    city_id varchar(4) NOT NULL,
+    city_name varchar(30) NOT NULL,
+    state_id varchar(4) NOT NULL,
+    PRIMARY KEY (city_id),
+    FOREIGN KEY (state_id) REFERENCES state_table(state_id)
+);
+
+CREATE TABLE IF NOT EXISTS zip_table (
+  zip_code varchar(5) NOT NULL,
+  city_id varchar(4) NOT NULL,
+  PRIMARY KEY (zip_code),
+  FOREIGN KEY (city_id) REFERENCES city_table(city_id)
+);
+
+CREATE TABLE IF NOT EXISTS hotel (
+    hotel_id varchar(8) NOT NULL,
+    hotel_name varchar(255) NOT NULL,
+    city_id varchar(4) NOT NULL,
+    PRIMARY KEY (hotel_id),
+    FOREIGN KEY (city_id) REFERENCES city_table(city_id)
+);
 
 CREATE TABLE IF NOT EXISTS invoice_bookings (
     invoice_id varchar(7),
     booking_id varchar(8) NOT NULL,
     booking_type varchar(8) NOT NULL, 
     PRIMARY KEY (invoice_id,booking_id)
+);
+
+CREATE TABLE IF NOT EXISTS calendar (
+    cal_date varchar(4) NOT NULL,
+    date_year varchar(30) NOT NULL,
+    date_month varchar(30) NOT NULL,
+    date_day varchar(30) NOT NULL,
+    date_quarter varchar(4) NOT NULL,
+    PRIMARY KEY (cal_date)
 );
 
 CREATE TABLE IF NOT EXISTS hotel_booking (
@@ -26,6 +71,16 @@ CREATE TABLE IF NOT EXISTS hotel_booking (
     FOREIGN KEY (hotel_end_date) REFERENCES calendar(cal_date)
 );
 
+CREATE TABLE IF NOT EXISTS flight_route (
+    route_id varchar(4) NOT NULL,
+    fl_startplace varchar(8) NOT NULL,
+    fl_endplace varchar(8) NOT NULL,
+    PRIMARY KEY (route_id),
+    FOREIGN KEY (fl_startplace) REFERENCES city_table(city_id),
+    FOREIGN KEY (fl_endplace) REFERENCES city_table(city_id)
+
+);
+
 CREATE TABLE IF NOT EXISTS flight_booking (
     invoice_id varchar(7) NOT NULL,
     booking_id varchar(7) NOT NULL,
@@ -37,59 +92,8 @@ CREATE TABLE IF NOT EXISTS flight_booking (
     PRIMARY KEY (booking_id),
     FOREIGN KEY (invoice_id,booking_id) REFERENCES invoice_bookings(invoice_id,booking_id),
     FOREIGN KEY (route_id) REFERENCES flight_route(route_id),
-    FOREIGN KEY (booking_date) REFERENCES calendar(date_id)
+    FOREIGN KEY (booking_date) REFERENCES calendar(cal_date)
     
-);
-
-CREATE TABLE IF NOT EXISTS hotel (
-    hotel_id varchar(8) NOT NULL,
-    hotel_name varchar(255) NOT NULL,
-    city_id varchar(4) NOT NULL,
-    PRIMARY KEY (hotel_id),
-    FOREIGN KEY (city_id) REFERENCES city_table(city_id)
-);
-
-
-
-
-CREATE TABLE IF NOT EXISTS flight_route (
-    route_id varchar(4) NOT NULL,
-    fl_startplace varchar(8) NOT NULL,
-    fl_endplace varchar(8) NOT NULL,
-    PRIMARY KEY (route_id),
-    FOREIGN KEY (fl_startplace) REFERENCES city_table(city_id),
-    FOREIGN KEY (fl_endplace) REFERENCES city_table(city_id)
-
-);
-
-CREATE TABLE IF NOT EXISTS zip_table (
-  zip_code varchar(5) NOT NULL,
-  city_id varchar(4) NOT NULL,
-  PRIMARY KEY (zip_code),
-  FOREIGN KEY (city_id) REFERENCES city_table(city_id)
-);
-
-
-CREATE TABLE IF NOT EXISTS city_table (
-    city_id varchar(4) NOT NULL,
-    city_name varchar(30) NOT NULL,
-    state_id varchar(4) NOT NULL,
-    PRIMARY KEY (city_id),
-    FOREIGN KEY (state_id) REFERENCES state_table(state_id)
-);
-
-CREATE TABLE IF NOT EXISTS state_table (
-    state_id varchar(4) NOT NULL,
-    state_name varchar(30) NOT NULL,
-    country_id varchar(4) NOT NULL,
-    PRIMARY KEY (state_id),
-    FOREIGN KEY (country_id) REFERENCES country_table(country_id)
-);
-
-CREATE TABLE IF NOT EXISTS country_table (
-    country_id varchar(4) NOT NULL,
-    country_name varchar(30) NOT NULL,
-    PRIMARY KEY (country_id)
 );
 
 CREATE TABLE IF NOT EXISTS customer_table(
@@ -102,16 +106,6 @@ CREATE TABLE IF NOT EXISTS customer_table(
     PRIMARY KEY (cust_id),
     FOREIGN KEY (cust_zip) REFERENCES zip_table(zip_code),
     FOREIGN KEY (cust_birthday) REFERENCES calendar(cal_date)
-);
-
-CREATE TABLE IF NOT EXISTS calendar (
-    cal_date varchar(4) NOT NULL,
-    date_year varchar(30) NOT NULL,
-    date_month varchar(30) NOT NULL,
-    date_day varchar(30) NOT NULL,
-    date_quarter varchar(4) NOT NULL,
-    PRIMARY KEY (state_id),
-    FOREIGN KEY (country_id) REFERENCES country_table(country_id)
 );
 
 
