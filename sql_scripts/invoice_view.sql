a6317b72f3a7df45caa78c86c116f7c5ff9ef923
Sven Goerdes <Sven@Goerdes.com>
2024-12-14 21:11:43 +0000

    invoice_view update

--- sql_scripts/invoice_view.sql
+++ sql_scripts/invoice_view.sql
@@ -1,4 +1,6 @@
-drop view if exists invoice_flights ;
+
+-- drop view if exists invoice_flights;
+
 CREATE VIEW invoice_flights AS 
 SELECT
 	"LSR" as company_name,
@@ -19,7 +21,7 @@ SELECT
      fb.booking_price
      
 	FROM 
-		invoice_bookings ib
+		inv_book_match ib
 		join customer_table ct
 		on ib.cust_id = ct.cust_id
         join zip_table zt
@@ -34,5 +36,52 @@ SELECT
         ON ib.invoice_id = fb.invoice_id AND ib.booking_id = fb.booking_id
         where ib.invoice_id='INV0002'
         ;
+  
     
-select * from invoice_flights
+
+
+
+
+
+-- drop view if exists invoice_flights;
+
+CREATE VIEW invoice_hotel AS 
+SELECT
+	"LSR" as company_name,
+    "Rua das Ameixoeiras 14, Campolide" as company_street,
+    "suporte@lsr.pt" as email,
+    "lsr.pt" as website,
+	ib.invoice_id,
+    ib.booking_type,
+    ib.booking_id,
+	ct.cust_first_name,
+    ct.cust_last_name,
+    ct.cust_address,
+    ct.cust_zip,
+    cit.city_name,
+    st.state_name,
+    cntr.country_name,
+	 fb.booking_date,
+     fb.booking_price
+     
+	FROM 
+		inv_book_match ib
+		join customer_table ct
+		on ib.cust_id = ct.cust_id
+        join zip_table zt
+        on zt.zip_code = ct.cust_zip
+        join city_table cit
+        on zt.city_id = cit.city_id
+        join state_table st 
+        on cit.state_id = st.state_id
+        join country_table cntr 
+        on st.country_id = cntr.country_id
+		JOIN hotel_booking fb
+        ON ib.invoice_id = fb.invoice_id AND ib.booking_id = fb.booking_id
+        where ib.invoice_id='INV0003'
+        ;
+  
+    
+
+select * from lsr.invoice_flights;
+select * from lsr.invoice_hotel;
